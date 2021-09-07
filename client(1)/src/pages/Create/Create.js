dfef9834f156bb8d84d1241eaa29218a65cd1abc
Hattori <65462973+Imhootep@users.noreply.github.com>
2021-09-07 16:41:31 +0200

    Add files via upload

---
+++ client(1)/src/pages/Create/Create.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../../components/Navbar'
+
+function Create() {
+    return (
+        <>
+        <Navbar/>
+        <div>
+            Create post, event or new profil
+        </div>
+        </>
+    )
+}
+
+export default Create
