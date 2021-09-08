2e80820d9a00a0df5d4d705a70acf3fe135168f8
Imhootep <fpbraibant@gmail.com>
2021-09-08 11:26:38 +0200

    create file

--- client/src/pages/Create/Create.js
+++ client/src/pages/Create/Create.js
@@ -1,25 +1,42 @@
 import React from 'react'
 import Navbar from '../../components/Navbar'
+import './Create.css'
 
 function Create() {
     return (
         <>
         <Navbar/>
-        <div>
-            <div className="create">
-               Poster un nouvel Event  
+        <div className="createModif">
+            <div className="createDoc">
+                <button className="post">
+                    <h3>Poster un nouvel Evènement</h3>
+                </button> 
             </div>
-            <div className="create">
-                Poster un nouveau Document
+            <div className="createDoc">
+                <button className="post">
+                    <h3>Poster un nouveau Document  </h3>
+                </button>
             </div>
-            <div className="create">
-                modifier Profil
+            <div className="createDoc">
+                <button className="post">
+                    <h3>Modifier Profil  </h3>
+                </button>
             </div>
-            <div className="create">
-                modifier Post
+            <div className="createDoc">
+                <button className="post">
+                    <h3>Modifier Post  </h3>
+                </button>
             </div>
-            <div></div>
+            
         </div>
+
+        <div>
+            <div>
+                <input type="text" placeholder="recherche d'Event" /> 
+                
+            </div>
+        </div>
+
         </>
     )
 }
