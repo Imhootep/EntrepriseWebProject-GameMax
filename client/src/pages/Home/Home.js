b7453df28b983fd7bb75732a8ca5c7bb5f9c7fd6
Imhootep <fpbraibant@gmail.com>
2021-09-09 14:53:12 +0200

    login redirect home

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import Navbar from '../../components/Navbar'
 
+
 function Home() {
+
+    // useEffect(()=>{
+    //     if(!localStorage.getItem("loggedIn")) {
+    //         localStorage.setItem("loggedIn", false);
+    //     }
+    // }, [])
+
     return (
         <>
         <Navbar/>
