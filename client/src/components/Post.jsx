dcf5ac2c30ee4a27cb6c7dbafb5a6c658db01e68
Guillaume <guiguifr@msn.com>
2021-09-10 09:50:08 +0200

    creation structure home, page post et css home

---
+++ client/src/components/Post.jsx
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from 'react'
+import '../pages/Home/Home.css'
+
+function Post() {
+
+
+    return (
+        <div className="post">   
+            <div>
+                ceci est un post
+            </div>
+        </div>
+    )
+}
+
+export default Post
--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,5 +1,6 @@
 import React, {useEffect} from 'react'
 import Navbar from '../../components/Navbar'
+import Post from '../../components/Post'
 
 
 function Home() {
@@ -13,9 +14,19 @@ function Home() {
     return (
         <>
         <Navbar/>
-        <div>
-            Home
+        <div className="containerHome">
+            <div className="flexCote">
+                Block de gauche
+            </div>
+            <div className="flexMiddle">
+                <Post/>
+            </div>
+            <div className="flexCote">
+                block de droite
+            </div>
+
         </div>
+        
 
         </>
     )
