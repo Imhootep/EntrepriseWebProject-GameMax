c0d861f18272ddf5f4a25d11bc795799e847043f
Guillaume <guiguifr@msn.com>
2021-09-13 14:56:45 +0200

    bloc post V2

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -38,17 +38,6 @@ import banner from '../img/banner.png' //testing
 
     }
 
-    /*
-    function Comment(){
-        const [isCommentDisplayed, setIsCommentDisplayed] = useState(false);
-        const handleClick = function (e){
-            e.preventDefault()
-            setIsCommentDisplayed(!isCommentDisplayed)
-        }
-    }
-    */
-    
-
     const ReadMore = ({ children }) => {
         const text = children;
         const [isReadMore, setIsReadMore] = useState(true);
@@ -67,33 +56,38 @@ import banner from '../img/banner.png' //testing
 
 
       const Post = () => {
-        return (
-            <div className="PostCOntainer">   
-            <div className="PostBlock">
-                <div className="PostAvatar">
-                    <img src={avatar} alt="avatar"/>
-                    <div className="AvatarName">Guillaume</div>
-                    <ul className="MoreInfos">
-                        <li>Posté le:</li>
-                        <li>10/09/2021</li>
-                    </ul>
-                </div>
-                <div className="PostContent">
-                    <img src={banner} className="banner" alt="banner"/>
-                    <div className="PostInfos">
-                        <div className="PostTitle">Le lorem ipsum</div>
-                        <div className="PostDate">22/09/2021</div>
+        return ( 
+            <div className="PostContainer">  
+               <div className="PostBlock2">
+                   <div className="BannerBlock">                   
+                        <div className="PostAvatar">
+                            <img src={avatar} alt="avatar"/>
+                            <div className="AvatarName2">Guillaume</div>
+                            <ul className="MoreInfos">
+                                <li>Posté le:</li>
+                                <li>10/09/2021</li>
+                            </ul>
+                        </div>
+                        <img src={banner} className="banner2" alt="banner"/>
                     </div>
-                    <ReadMore>
-                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum. Maecenas adipiscing ante non diam sodales hendrerit.
+                    <div className="TitleBlock">
+                        <div className="PostTitle2">Le lorem ipsum</div>
+                        {/*<div className="PostInfos">
+                            <div className="PostDate">22/09/2021</div>
+                        </div>*/}
+                        </div>
+                        <div className="PostContent2">
+                            <ReadMore>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum. Maecenas adipiscing ante non diam sodales hendrerit.
+Ut velit mauris, egestas sed, gravida nec, ornare ut, mi. Aenean ut orci vel massa suscipit pulvinar. Nulla sollicitudin. Fusce varius, ligula non tempus aliquam, nunc turpis ullamcorper nibh, in tempus sapien eros vitae ligula. Pellentesque rhoncus nunc et augue. Integer id felis. Curabitur aliquet pellentesque diam. Integer quis metus vitae elit lobortis egestas. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Morbi vel erat non mauris convallis vehicula. Nulla et sapien. Integer tortor tellus, aliquam faucibus, convallis id, congue eu, quam. Mauris ullamcorper felis vitae erat. Proin feugiat, augue non elementum posuere, metus purus iaculis lectus, et tristique ligula justo vitae magna.
 
-                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum. Maecenas adipiscing ante non diam sodales hendrerit.
-                    </ReadMore>
-                    <Comment />
-                    
+Aliquam convallis sollicitudin purus. Praesent aliquam, enim at fermentum mollis, ligula massa adipiscing nisl, ac euismod nibh nisl eu lectus. Fusce vulputate sem at sapien. Vivamus leo. Aliquam euismod libero eu enim. Nulla nec felis sed leo placerat imperdiet. Aenean suscipit nulla in justo. Suspendisse cursus rutrum augue. Nulla tincidunt tincidunt mi. Curabitur iaculis, lorem vel rhoncus faucibus, felis magna fermentum augue, et ultricies lacus lorem varius purus. Curabitur eu amet. 
+                            </ReadMore>
+                            <Comment />
+                        </div>
                 </div>
             </div>
-        </div>
+
         );
       };
 
