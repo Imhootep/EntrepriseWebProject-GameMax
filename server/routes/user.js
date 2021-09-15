723bbc58a17381700986e9bab33d50c2a00e8302
Flo-17 <cammarata.florian@hotmail.com>
2021-09-15 10:08:56 +0200

    Passport local OK

--- server/routes/user.js
+++ server/routes/user.js
@@ -24,7 +24,21 @@ passport.use(new LocalStrategy(
             }
         });
     }
-  ));
+));
+/*
+  passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
+    User.findOne({id: jwt_payload.sub}, function(err, user) {
+        if (err) {
+            return done(err, false);
+        }
+        if (user) {
+            return done(null, user);
+        } else {
+            return done(null, false);
+            // or you could create a new account
+        }
+    });
+}));*/
   
 passport.serializeUser(function(user, done) {
     done(null, user.id);
