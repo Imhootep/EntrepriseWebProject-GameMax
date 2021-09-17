96d124887da97e57f344b1933f6cb9db68fa55b4
Flo-17 <cammarata.florian@hotmail.com>
2021-09-17 14:58:28 +0200

    Nettoyage userRoute / server (flo)

--- server/server.js
+++ server/server.js
@@ -21,8 +21,6 @@ app.listen(process.env.PORT, () => {
 
 const userRoute = require('./routes/userRoute')
 app.use ("/", userRoute);
-// const postRoute = require('./routes/postRoute')
-// app.use("/", postRoute);
 const commentRoute = require('./routes/commentRoute')
 app.use("/", commentRoute);
 const uploadRoute = require('./routes/uploadRoute')
