17e0a1462c21bc84043afcf8bc3804c3b9e963e0
Imhootep <fpbraibant@gmail.com>
2021-09-21 13:34:42 +0200

    recup profil

--- client/src/pages/Profil/Profil.js
+++ client/src/pages/Profil/Profil.js
@@ -7,7 +7,7 @@ function Profil() {
     const [users, setUsers] = useState([])
 
     useEffect(() =>{
-        Axios.get("http://localhost:8000/user/id").then ((response) =>{
+        Axios.get("http://localhost:8000/user").then ((response) =>{
             setUsers(response.data);
             
             console.log(response.data);
@@ -23,11 +23,13 @@ function Profil() {
         {users.map((val)=>{
                     return(
           <div>
+              <div className="postProfil" key={val.id}>
             <div>{val.username }</div>
             <div>{val.email }</div>
             <div>{val.phone }</div>
             <div>{val.street }</div>
             <div>{val.website }</div>
+            </div>
             </div>
           
          )
